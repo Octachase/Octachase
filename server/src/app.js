ff0e0f3a2dd611d3fa63a876a6180ee028d098b6
octachase <octachase@gmail.com>
2024-01-26 08:27:31 -0800

    updated code

--- server/src/app.js
+++ server/src/app.js
@@ -12,9 +12,21 @@ app.use(express.json());
 
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "https://astro-ebon-six.vercel.app"],
+		origin: [process.env.FRONTEND_URL],
 	})
 );
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "text/plain, Domain");
+
+	// Handle preflight requests
+	if (req.method === "OPTIONS") {
+		res.sendStatus(200);
+	} else {
+		next(); // Continue with the main request
+	}
+});
 
 app.use((req, res, next) => {
 	if (!req.headers.domain || req.headers.domain !== process.env.BACKEND_URL) {
